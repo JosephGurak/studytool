28a1d32a3389e08184a14b5e213e1ec401ceda6a
Joseph Gurak <josephgurak@outlook.com>
2025-11-19 01:12:59 -0500

    [update]
    work on structure

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,6 +3,7 @@
 use std::io;
 use std::collections::HashMap;
 
+
 #[derive(Debug)]
 struct FlashCardBack {
     answer: String
@@ -10,36 +11,58 @@ struct FlashCardBack {
 
 #[derive(Eq,PartialEq,Hash,Debug)]
 struct FlashCardFront {
-    topic: String
+    category: String,
+    question: String
 }
 
-struct MainHashMap {
-    nested_hashmap: HashMap<String, HashMap<FlashCardFront,FlashCardBack>>
+
+#[derive(Debug)]
+struct AwsFlashcards {
+    card: HashMap<FlashCardFront, FlashCardBack>
 }
 
+impl AwsFlashcards {
+    fn new() -> Self {
+        Self {
+            card: HashMap::new(),
+        }
+    }
+
+    fn add_card(&mut self, key: FlashCardFront, value: FlashCardBack) {
+        self.card.insert(key,value);
+    }
+
+    fn get_answer(&self, key: &FlashCardFront) -> Option<&FlashCardBack> {
+        self.card.get(key)
+    }
+
+}
 
 
 // have user input menu for studying terms and topics in rust, AWS, Datastructs & Algorithms
 // make nice CLI app for personal use
 // use rand crate to mix up cards in a topic.
 fn main() {
-    let mut flashcards = MainHashMap{ nested_hashmap: HashMap::new()};
 
-    
-    let mut aws_flashcards: HashMap<FlashCardFront, FlashCardBack> = HashMap::new();
-    aws_flashcards.insert(
-        FlashCardFront {
-            topic: "What is EC2".to_owned()
-        },
-        FlashCardBack {
+   // let mut aws_flashcards: HashMap<FlashCardFront, FlashCardBack> = HashMap::new();
+    let mut aws_flashcards = AwsFlashcards::new();
+
+    let front = FlashCardFront {
+            category: "cloud concepts".to_owned(),
+            question: "What is EC2".to_owned()
+    };
+    let back = FlashCardBack {
             answer: "Elastic Compute Cloud".to_owned()
-        }
-    );
+    };
+
+    aws_flashcards.add_card(front,back);
 
 
-    for (key, value) in aws_flashcards {
-        println!("{:?} {:?}",key,value);
-    }
-
+    // work on better structture for this and searching via category or question
+    let question = FlashCardFront {
+        category: "cloud concepts".to_owned(),
+        question: "What is EC2".to_owned(),
+    };
+    println!("{:?}", aws_flashcards.get_answer(&question));
 
 }
